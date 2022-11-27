478ec1304cb6d9cc0f6b9f420cb34ccfe0fe2ade
Elizabeth Levkovic <71608650+liza-vic@users.noreply.github.com>
2022-11-27 20:02:13 +0300

    Add files via upload

---
+++ UnderarmourTesting/src/test/java/test/HomePageTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.HomePage;
+
+    public class HomePageTest {
+        private WebDriver driver;
+
+
+
+    @BeforeMethod(
+            alwaysRun = true
+    )
+    public void browserSetUp() {
+        this.driver = new OperaDriver();
+    }
+
+    @Test
+    public void checkValidDataInTextArea() {
+        String expectedMessage = "Sorry, no results for “---”";
+        HomePage homePage = (new HomePage(this.driver)).openPage().clickInputBoxMainPage()
+                .writeSomeWordsIntoInputBoxMainPage("---")
+                .pressEnter().inputInEmailField("no").closeWindow();
+        String actualMessage = homePage.isContainsErrorElement();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @AfterMethod(
+            alwaysRun = true
+    )
+    public void browserTearDown() {
+        this.driver.quit();
+    }
+}
---
+++ UnderarmourTesting/src/test/java/test/TrackAnOrderTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.TrackAnOrderPage;
+
+public class TrackAnOrderTest {
+    private WebDriver driver;
+
+
+    @BeforeMethod(
+            alwaysRun = true
+    )
+    public void browserSetUp() {
+        this.driver = new ChromeDriver();
+    }
+
+    @Test
+    public void checkValidDataInTextArea() throws InterruptedException {
+        String expectedMessage = "Sorry! We’re experiencing a system issue and cannot retrieve your order right now. Please try back later.";
+        TrackAnOrderPage trackAnOrderPage = (new TrackAnOrderPage(this.driver)).openPage().enterOrderNumbers("12356432")
+                .inputInEmailField("liza.azazi@gmail.com").pressEnter();
+        String actualMessage = trackAnOrderPage.isContainsErrorElement();
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @AfterMethod(
+            alwaysRun = true
+    )
+    public void browserTearDown() {
+        this.driver.quit();
+    }
+}
