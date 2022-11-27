7607bb5f1a8747a49e42a1ebaacd733a7b60d89a
Elizabeth Levkovich <ea.levkovic@gmail.com>
2022-11-27 23:42:49 +0300

    10 lab

---
+++ UnderarmourTesting/src/test/java/page/TrackAnOrderPage.java
@@ -0,0 +1,55 @@
+package page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TrackAnOrderPage extends AbstractPage{
+    private static final String PAGE_URL = "https://www.underarmour.com/en-us/guest-login/";
+    private static final String PAGE_FINITE_URL = "https://www.underarmour.com/on/demandware.store/Sites-US-Site/en_US/Order-Track/";
+    private static final String FIND_TRACK_ORDER_FORM_NUMBER = "//*[@id='trackorder-form-number']";
+    private static final String FIND_TRACK_ORDER_FORM_EMAIL = "//*[@id='trackorder-form-email']";
+    private static final String ERROR_XPATH = "//*[@id='maincontent']/div/div/div/div[1]/form/div[2]/div[1]/div/strong";
+
+    @FindBy(xpath = FIND_TRACK_ORDER_FORM_NUMBER)
+    private WebElement inputFormOderNumber;
+
+    @FindBy(xpath = FIND_TRACK_ORDER_FORM_EMAIL)
+    private WebElement inputEmailBox;
+
+    public TrackAnOrderPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public TrackAnOrderPage openPage() {
+        driver.get(PAGE_URL);
+        return this;
+    }
+
+    public TrackAnOrderPage enterOrderNumbers(String term) {
+        inputFormOderNumber.click();
+        inputFormOderNumber.sendKeys(term);
+        return this;
+    }
+
+    public TrackAnOrderPage inputInEmailField(String term) {
+        inputEmailBox.click();
+        inputEmailBox.sendKeys(term);
+        return this;
+    }
+    public TrackAnOrderPage pressEnter() {
+        inputEmailBox.sendKeys(Keys.ENTER);
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(PAGE_FINITE_URL)));
+        return this;
+    }
+    public String isContainsErrorElement() {
+        return driver.findElement(By.xpath(ERROR_XPATH)).getText();
+    }
+
+
+}
---
+++ UnderarmourTesting/src/test/java/test/HomePageTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page.HomePage;
+
+public class HomePageTest {
+    private WebDriver driver;
+
+    public HomePageTest() {
+    }
+
+    @BeforeMethod(
+            alwaysRun = true
+    )
+    public void browserSetUp() {
+        this.driver = new ChromeDriver();
+    }
+
+    @Test
+    public void checkValidDataInTextArea() throws InterruptedException {
+        String expectedMessage1 = "Sorry, no results for “---”";
+        HomePage homePage = (new HomePage(this.driver)).openPage().clickInputBoxMainPage()
+                .writeSomeWordsIntoInputBoxMainPage("---")
+                .pressEnter().inputInEmailField("no").closeWindow();
+        String actualMessage = homePage.isContainsErrorElement();
+        Assert.assertEquals(expectedMessage1, actualMessage);
+    }
+
+    @AfterMethod(
+            alwaysRun = true
+    )
+    public void browserTearDown() {
+        this.driver.quit();
+    }
+}
