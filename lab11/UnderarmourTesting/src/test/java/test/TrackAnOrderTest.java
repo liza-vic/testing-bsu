4decbefffbf0329402bcfd5c7c0b0a87ca44cc88
Elizabeth Levkovic <71608650+liza-vic@users.noreply.github.com>
2022-12-15 10:08:10 +0300

    Update TrackAnOrderTest.java

--- lab11/UnderarmourTesting/src/test/java/test/TrackAnOrderTest.java
+++ lab11/UnderarmourTesting/src/test/java/test/TrackAnOrderTest.java
@@ -1,15 +1,18 @@
 package test;
 
 import model.UserData;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import page.TrackAnOrderPage;
 import service.UserDataCreator;
+import util.TestListener;
 
 import static test.HomePageTest.TESTDATA_USER_POPUP_EMAIL_FORM;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+@Listeners({TestListener.class})
 public class TrackAnOrderTest extends CommonConditions {
 
     @Test
