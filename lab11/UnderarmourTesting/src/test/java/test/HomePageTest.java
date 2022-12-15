c5f8a07d4596275f792ecc0957b0c5f755ad454b
Elizabeth Levkovic <71608650+liza-vic@users.noreply.github.com>
2022-12-15 10:08:32 +0300

    Update HomePageTest.java

--- lab11/UnderarmourTesting/src/test/java/test/HomePageTest.java
+++ lab11/UnderarmourTesting/src/test/java/test/HomePageTest.java
@@ -1,13 +1,16 @@
 package test;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import page.HomePage;
 import service.TestDataReader;
+import util.TestListener;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+@Listeners({TestListener.class})
 public class HomePageTest extends CommonConditions {
     public static final String TESTDATA_SEARCH_REQUEST = TestDataReader.getTestData("testdata.search.request");
     public static final String TESTDATA_USER_POPUP_EMAIL_FORM = TestDataReader.getTestData("testdata.user.popup.email.form");
